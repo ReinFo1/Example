0f5816e66af114aa266797c4c007bfb3b01b8680
ReinFo1 <kubarev.vadim.r@gmail.com>
2020-11-06 15:24:35 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,60 +4,84 @@ namespace Z1_isrpo
 {
         class Program
         {
-            static void Main(string[] args)
+        static void Main(string[] args)
+        {
+            int M2, M11, M22, R, MAX, dat, res, i, N = 0;
+            M2 = 0;
+            M11 = 0;
+            M22 = 0;
+            MAX = 0;
+            try
             {
-                int M2, M11, M22, R, MAX, dat, res, i, N;
-                M2 = 0;
-                M11 = 0;
-                M22 = 0;
-                MAX = 0;
                 N = Convert.ToInt32(Console.ReadLine());
-                for (i = 1; i <= N; i++)
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            for (i = 1; i <= N; i++)
+            {
+                try
                 {
                     dat = Convert.ToInt32(Console.ReadLine());
-                    if ((dat % 2 == 0) && ((dat % 11) > 0) && (dat > M2))
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                if ((dat % 2 == 0) && ((dat % 11) > 0) && (dat > M2))
+                {
+                    M2 = dat;
+                }
+                if ((dat % 11 == 0) && (dat % 2) > 0 && (dat > M11))
+                {
+                    M11 = dat;
+                }
+                if ((dat % 22) == 0 && (dat > M22))
+                {
+                    if (M22 > MAX)
                     {
-                        M2 = dat;
+                        MAX = M22;
+                        M22 = dat;
                     }
-                    if ((dat % 11 == 0) && (dat % 2) > 0 && (dat > M11))
+                    else
                     {
-                        M11 = dat;
-                    }
-                    if ((dat % 22) == 0 && (dat > M22))
-                    {
-                        if (M22 > MAX)
+                        if (dat > MAX)
                         {
-                            MAX = M22;
-                            M22 = dat;
-                        }
-                        else
-                        {
-                            if (dat > MAX)
-                            {
-                                MAX = dat;
-                            }
+                            MAX = dat;
                         }
                     }
                 }
-                R = Convert.ToInt32(Console.ReadLine());
-                if (M2 * M22 < M22 * MAX)
-                {
-                    res = M22 * MAX;
-                }
-                else
-                {
-                    res = M2 * M11;
-                }
-                Console.WriteLine($"Вычисленное контрольное значение: {res}");
-                if (R == res)
-                {
-                    Console.WriteLine("Контроль пройден.");
-                }
-                else
-                {
-                    Console.WriteLine("Контроль не пройден.");
-                }
-                Console.ReadKey();
             }
+            try
+            {
+                R = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            if (M2 * M22 < M22 * MAX)
+            {
+                res = M22 * MAX;
+            }
+            else
+            {
+                res = M2 * M11;
+            }
+            Console.WriteLine($"Вычисленное контрольное значение: {res}");
+            if (R == res)
+            {
+                Console.WriteLine("Контроль пройден.");
+            }
+            else
+            {
+                Console.WriteLine("Контроль не пройден.");
+            }
+            Console.ReadKey();
         }
+    }
 }
